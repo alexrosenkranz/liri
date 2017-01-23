bde3489567b8fa4683aa476c85102249805d486f
Alex Rosenkranz <alexrosenkranz@Alexs-iMac.local>
2017-01-23 14:44:23 -0500

    Writes to log.txt, minor bug with twitter I need to figure out

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var key = require('./keys.js');
+var fs = require('fs');
 
 var command = process.argv[2];
 var searchName = process.argv[3]
@@ -7,12 +8,18 @@ var searchName = process.argv[3]
 function tweets() {
   var params = {screen_name: 'alexrosenkranz', count: 20};
 
-  key.twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if(error) throw error;
-    // console.log(tweets);  // The favorites. 
+  key.twitterKeys.get('statuses/user_timeline', params, function(error, tweets, respond) {
+    if(error) throw error; 
+    var response;
     for (var i = 0; i < 20; i++) {
-      console.log(tweets[i].created_at + '\n' + tweets[i].text + '\n');
+      response += tweets[i].created_at + "\n" + tweets[i].text + "\n\n";
     }
+    console.log(response);
+    fs.appendFile("log.txt", "Recent Tweets \n\n" + response + "\n\n------------\n\n", function(err) {
+      if(err) {
+        return console.log(err);
+      }
+    });
   });
   
 }
@@ -25,13 +32,18 @@ function spotify() {
   }
 
   spotify.search({ type: 'track', query: searchName }, function(err, data) {
-      if ( err ) {
-          console.log('Error occurred: ' + err);
-          return;
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    var response = 'Artist: ' + data.tracks.items[0].artists[0].name + '\nSong Name: ' + data.tracks.items[0].name + '\nFrom Album: ' + data.tracks.items[0].album.name + '\nPreview: ' + data.tracks.items[0].preview_url;
+    console.log(response);
+    fs.appendFile("log.txt", "Song Request \n\n" + response + "\n\n------------\n\n", function(err) {
+      if(err) {
+        return console.log(err);
       }
-      console.log('Artist: ' + data.tracks.items[0].artists[0].name + '\nSong Name: ' + data.tracks.items[0].name + '\nFrom Album: ' + data.tracks.items[0].album.name + '\nPreview: ' + data.tracks.items[0].preview_url + '\n');
     });
-
+  });
 
 }
 
@@ -44,7 +56,15 @@ function movie() {
   request.get('http://www.omdbapi.com/?r=json&tomatoes=true&t=' + searchName, function (error, response, movie) {
     if (!error && response.statusCode == 200) {
       movie = JSON.parse(movie);
-      console.log(movie.Title + '\nYear: ' + movie.Year + '\nIMDB Rating: ' + movie.imdbRating + '\nCountry: ' + movie.Country + '\nLanguage: ' + movie.Language + '\nPlot: ' + movie.Plot + '\nActors: ' + movie.Actors + '\nRotten Tomatoes Rating: ' + movie.tomatoUserRating + '\nRotten Tomatoes URL: ' + movie.tomatoURL);
+      var response = movie.Title + '\nYear: ' + movie.Year + '\nIMDB Rating: ' + movie.imdbRating + '\nCountry: ' + movie.Country + '\nLanguage: ' + movie.Language + '\nPlot: ' + movie.Plot + '\nActors: ' + movie.Actors + '\nRotten Tomatoes Rating: ' + movie.tomatoUserRating + '\nRotten Tomatoes URL: ' + movie.tomatoURL;
+      
+      console.log(response);
+
+      fs.appendFile("log.txt", "Movie Request \n\n" + response + "\n\n------------\n\n", function(err) {
+      if(err) {
+        return console.log(err);
+      }
+    });
     }
   })
 }
@@ -75,6 +95,7 @@ function says() {
 switch (command) {
   case "my-tweets":
     tweets();
+
     break;
   case "spotify-this-song":
     spotify();
